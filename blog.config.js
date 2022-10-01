6e5f6f556832d96ea1c3b86989bf1e4b4efd1121
Xiangxuan Qu <quxiangxuanqxx@gmail.com>
2022-10-01 16:16:26 +0800

    fix: some links

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://www.linkedin.com/in/fghpdf/',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', '日本', '新加坡', '润', 'Japan', 'Singapore', 'Xiangxuan', 'fghpdf'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
